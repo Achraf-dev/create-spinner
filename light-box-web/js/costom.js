27803f8213b087ad1072b1d8d6c068491619179f
Achraf-dev <43117469+Achraf-dev@users.noreply.github.com>
2019-03-06 19:18:04 +0100

    Add files via upload

---
+++ light-box-web/js/costom.js
@@ -0,0 +1,16 @@
+/* global $, alert, console*/
+$(function(){
+
+'use strict';
+//trigger(ma3neha ykhadem) nice scroll pluggin
+  $('html').niceScroll();
+  
+});
+$("html").niceScroll({
+  cursorcolor:"#F46124",
+  cursorwidth: "7px"
+  
+    
+});
+
+
